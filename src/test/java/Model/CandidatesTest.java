9f8fb82fcac89a988111f83484b66613c5841630
111598094 <y74185296301999@gmail.com>
2023-04-26 17:08:29 +0800

    CandidatesTest init

---
+++ src/test/java/Model/CandidatesTest.java
@@ -0,0 +1,21 @@
+package Model;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.*;
+
+import static org.junit.Assert.*;
+
+public class CandidatesTest {
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    @After
+    public void tearDown() throws Exception {
+    }
+
+
+}
